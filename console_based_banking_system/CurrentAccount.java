6935f066db346cf887cd44ea276bd92602d4ff1f
JateenDhaduk <jateendhaduk456@gmail.com>
2026-03-02 13:09:10 +0000

    override the withdraw method

--- console_based_banking_system/CurrentAccount.java
+++ console_based_banking_system/CurrentAccount.java
@@ -1,5 +1,15 @@
 public class CurrentAccount extends Account {
+    private final double overdraftLimit = -10000; // Overdraft limit of 10,000
     public CurrentAccount(String personName , String accountType , String accountNo , String personAddress , String personPhoneNo){
         super(personName , accountType , accountNo , personAddress , personPhoneNo);
-    })
-}
+    }
+
+    @Override
+    public void withdraw(double amount) throws IllegalArgumentException {
+        if (amount > 0 && overdraftLimit <= (getBalance() - amount)) {
+            setBalance(getBalance() - amount);       
+        } else {
+            throw new IllegalArgumentException("Invalid withdrawal amount or overdraft limit exceeded.");
+        }
+    }
+}    

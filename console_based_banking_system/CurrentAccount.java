696ed76870152e1c11bc75f7fc2898ed36238d62
JateenDhaduk <jateendhaduk456@gmail.com>
2026-03-02 12:10:02 +0000

    Add constructor to CurrentAccount to initialize fields via superclass

--- console_based_banking_system/CurrentAccount.java
+++ console_based_banking_system/CurrentAccount.java
@@ -1,3 +1,5 @@
 public class CurrentAccount extends Account {
-    
+    public CurrentAccount(String personName , String accountType , String accountNo , String personAddress , String personPhoneNo){
+        super(personName , accountType , accountNo , personAddress , personPhoneNo);
+    })
 }
